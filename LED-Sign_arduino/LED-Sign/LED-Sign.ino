5c4dc85f273299990c3ccca4efaa1c891d9aaeb2
Chris Ostmo <chris@appideas.com>
2018-09-03 14:37:37 -0700

    - Add NTP for future use (turn lights on and off at specified times) - Better comments

--- LED-Sign_arduino/LED-Sign/LED-Sign.ino
+++ LED-Sign_arduino/LED-Sign/LED-Sign.ino
@@ -1,6 +1,20 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <ArduinoOTA.h>
+#include <WiFiUdp.h>
+
+/**
+ * A sketch to control two strings of RGBW LED lights from one ESP8266
+ * 
+ * @author costmo
+ * @since  20180902
+ */
+
+const char* ssid     = "my-network-ssid";
+const char* password = "my-network-password";
+const int TIMEZONE_OFFSET = (-1 * (8 * 3600)); // Currently GMT-8
+const char* ntpServerName = "time.nist.gov";
+const int checkInterval = 60 * 30; // how often (in seconds) to query the NTP server for updates (30 minutes)
 
 // For reference (pin mapping for ESP8266-12E):
 // D0/GPIO16      = 16;
@@ -29,8 +43,6 @@
 int maxBrightness = 1024;
 int offBrightness = 0;
 
-const char* ssid     = "ostmo";
-const char* password = "stikaa11";
 int serverPort = 80;
 int status = WL_IDLE_STATUS;
 IPAddress ip;
@@ -52,6 +64,29 @@ int currentGSecond = 0;
 int currentBSecond = 0;
 int currentWSecond = 0;
 
+// NTP setup variables
+
+unsigned int localPort = 2390;      // local port to listen for UDP packets
+IPAddress timeServerIP; // time.nist.gov NTP server address
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP udp;
+
+String fullTime = String( "" );
+String humanTime = String( "" );
+int intTime = 0;
+int currentHour = 0;
+int currentMinute = 0;
+int currentSecond = 0;
+int currentMeridiem = 0;
+
+/**
+ * Run on device power-on
+ * 
+ * @author costmo
+ * @since  20180902
+ */
 void setup() 
 {
   Serial.begin( 57600 );
@@ -70,18 +105,55 @@ void setup()
   connectToWifi();
   initOta();
   server.begin(); // Start the API server/service
+
+  // turn all the lights on when the ESP first powers on
+  turnOn( "first" );
+  turnOn( "second" );
+
+  // Get and show the time
+  Serial.println( "Starting UDP" );
+  udp.begin( localPort );
+  fullTime = getTime();
+  timeToVars();
+  Serial.println( "Current time: " + humanTime );
 }
 
+/**
+ * Turn all lights to max brightness
+ * 
+ * @return void
+ * @author costmo
+ * @since  20180902
+ * @param  String   whichPosition     first|second
+ */
 void turnOn( String whichPosition ) 
 { 
   setColorToLevel( whichPosition, "all", maxBrightness );
 }
 
+/**
+ * Turn all lights off
+ * 
+ * @return void
+ * @author costmo
+ * @since  20180902
+ * @param  String   whichPosition     first|second
+ */
 void turnOff( String whichPosition ) 
 {
   setColorToLevel( whichPosition, "all", offBrightness );
 }
 
+/**
+ * Set a color at a position to a specific level between 0 and 1024
+ * 
+ * @return void
+ * @author costmo
+ * @since  20180902
+ * @param  String   whichPosition     first|second
+ * @param  String   color             red|green|blue|white|all
+ * @param  intng    level             0 - 1024
+ */
 void setColorToLevel( String whichPosition, String color, int level ) 
 { 
 
@@ -146,52 +218,26 @@ void setColorToLevel( String whichPosition, String color, int level )
   }
 }
 
+/**
+ * Run while the sketch is active
+ * 
+ * @return void
+ * @author costmo
+ * @since  20180902
+ */
 void loop() 
 {
   ArduinoOTA.handle();
   nodeServer();
 }
 
-
-int parseOtaRequest( String requestString )
-{
-  char *line;
-  char *token;
-  char *tokenOut;
-  char *finalToken;
-  char *currentString = (char *)requestString.c_str();
-  String myString = String( currentString );
-  
-  line = strtok( currentString, "\n" );
-  while( line != NULL )
-  {
-    if( String( line ).indexOf( "?action=otasend" ) > 0 )
-    {
-      token = tokenOut = strtok( line, "-" );
-      while( token != NULL )
-      {
-        token = strtok( NULL, "-") ;
-        if( token != NULL )
-        {
-          tokenOut = token;
-        } 
-      }
-      break;
-    }
-    line = strtok( NULL, "\n") ;
-  }
-  
-  if( tokenOut != NULL )
-  {
-     finalToken = strtok( tokenOut, " " );
-  }
-
-  return String( finalToken ).toInt();
-}
-
-
-
-
+/**
+ * Connect to a wifi network and print the status
+ * 
+ * @return void
+ * @author costmo
+ * @since  20180902
+ */
 void connectToWifi()
 {
   Serial.print( "Connecting to " );
@@ -210,6 +256,13 @@ void connectToWifi()
   printWifiStatus();
 }
 
+/**
+ * Print the current IP address
+ * 
+ * @return void
+ * @author costmo
+ * @since  20180902
+ */
 void printWifiStatus() 
 {
   // print the SSID of the network you're attached to:
@@ -228,6 +281,14 @@ void printWifiStatus()
 //  Serial.println (" dBm" );
 }
 
+/**
+ * Prints the HTML header for output
+ * 
+ * @return void
+ * @author costmo
+ * @since  20180902
+ * @param  WifiClient   client    The wifi client
+ */
 void htmlHeader( WiFiClient client )
 {
     client.println( "HTTP/1.1 200 OK" );
@@ -239,12 +300,26 @@ void htmlHeader( WiFiClient client )
     client.println( "<html>" );
 }
 
+ /**
+ * Prints the HTML footer for output
+ * 
+ * @return void
+ * @author costmo
+ * @since  20180902
+ * @param  WifiClient   client    The wifi client
+ */
 void htmlFooter( WiFiClient client )
 {
     client.println( "</html>" );
 }
 
-
+/**
+ * Initialize OTA communication
+ * 
+ * @return void
+ * @author costmo
+ * @since  20180902
+ */
 void initOta()
 {
   ArduinoOTA.onStart( []() 
@@ -284,7 +359,16 @@ void initOta()
   });
   ArduinoOTA.begin();
 }
-
+ 
+/**
+ * Gets the current level (0 - 1024) for the given color and position
+ * 
+ * @return int
+ * @author costmo
+ * @since  20180902
+ * @param  String   whichPosition     first|second
+ * @param  String   color             red|green|blue|white|all
+ */
 int getLevelForColor( String whichPostiion, String color )
 {
   
@@ -338,13 +422,29 @@ int getLevelForColor( String whichPostiion, String color )
   }
 }
 
+// 
+/**
+ * Gets the current ratio (0.0 - 1.0) for the given color and position
+ * 
+ * @return float
+ * @author costmo
+ * @since  20180902
+ * @param  String   whichPosition     first|second
+ * @param  String   color             red|green|blue|white|all
+ */
 float getRatioForColor( String whichPosition, String color )
 {
   int  currentValue = getLevelForColor( whichPosition, color );
   return (float) ((float)currentValue / (float)1024);
 }
 
-// A tiny API server for a farm master node
+/**
+ * A tiny API server for receiving and handling requests
+ * 
+ * @return void
+ * @author costmo
+ * @since  20180902
+ */
 void nodeServer()
 {
    WiFiClient client = server.available();
@@ -486,6 +586,13 @@ void nodeServer()
    } // if( client )
 } // void nodeServer()
 
+/**
+ * Gets an HTML string with the values for all lights and positions
+ * 
+ * @return String
+ * @author costmo
+ * @since  20180902
+ */
 String getValues()
 {
   String output = "<p>First Red value: ";
@@ -545,5 +652,253 @@ String getValues()
                 output += "&nbsp;</p>";
 
   return output;
+}
+
+/**
+ * Get the current time from an NTP server
+ * 
+ * @return String
+ * @author costmo
+ * @since  20180902
+ */
+String getTime()
+{
+  
+  //Serial.println( "Getting time" );
+  String returnValue = String();
+
+  //get a random server from the pool
+  WiFi.hostByName( ntpServerName, timeServerIP );
+
+  sendNTPpacket( timeServerIP ); // send an NTP packet to a time server
+  // wait to see if a reply is available
+  delay( 2000 );
+  
+  int cb = udp.parsePacket();
+  // loop until we have a good connection
+  while( !cb ) 
+  {
+    //Serial.println( "Problem reading from NTP server. Retrying in 10 seconds..." );
+    delay( 10000);
+    //Serial.println( "Trying again..." );
+    cb = udp.parsePacket();
+  }
+
+//    Serial.print( "packet received, length=" );
+//    Serial.println( cb );
+    // We've received a packet, read the data from it
+    udp.read( packetBuffer, NTP_PACKET_SIZE ); // read the packet into the buffer
+
+    //the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, esxtract the two words:
+
+    unsigned long highWord = word( packetBuffer[40], packetBuffer[41] );
+    unsigned long lowWord = word( packetBuffer[42], packetBuffer[43] );
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+//    Serial.print( "Seconds since Jan 1 1900 = " );
+//    Serial.println( secsSince1900 );
+
+    // now convert NTP time into everyday time:
+//    Serial.print( "Unix time = " );
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const unsigned long seventyYears = 2208988800UL;
+    // subtract seventy years:
+    unsigned long epoch = secsSince1900 - seventyYears;
+    // print Unix time:
+//    Serial.println( epoch );
+
+    epoch = epoch + TIMEZONE_OFFSET;
+//    Serial.print( "The local epoch time is " + epoch );
+//    Serial.println( epoch );
+
+
+    // print the hour, minute and second:
+    //Serial.print( "The local time is " );       // UTC is the time at Greenwich Meridian (GMT)
+
+    int hour = ((epoch  % 86400L) / 3600);
+    int minute = ((epoch  % 3600) / 60);
+    int second = (epoch % 60);
+    
+    // make a 12 hour clock
+    bool isPM = false;
+    if( hour >= 12 )
+    {
+      isPM = true;
+      if( hour > 12 )
+      {
+        hour = hour - 12;
+      }
+    }
+    if( hour == 0 )
+    {
+      hour = 12;
+    }
+    
+    returnValue = String( hour );
+    returnValue = String( returnValue + " " );
+    returnValue = String( returnValue + minute );
+    returnValue = String( returnValue + " " );
+    returnValue = String( returnValue + second );
+    
+    if( isPM )
+    {
+      returnValue = String( returnValue + " 1" );
+    }
+    else
+    {
+      returnValue = String( returnValue + " 0" );
+    }
+    
+//    Serial.print( hour ); // print the hour (86400 equals secs per day)
+//    Serial.print(':');
+//    Serial.print( minute ); // print the minute (3600 equals secs per minute)
+//    Serial.print(':');
+//    Serial.println( second ); // print the second
+//    Serial.print( "RV: " );
+//    Serial.println( returnValue );
+
+    humanTime = String( hour );
+    humanTime = String( humanTime + ":" );
+    humanTime = String( humanTime + minute );
+    humanTime = String( humanTime + ":" );
+    humanTime = String( humanTime + second );
+    if( isPM )
+    {
+      humanTime = String( humanTime + " PM" );
+    }
+    else
+    {
+      humanTime = String( humanTime + " AM" );
+    }
+
+  return returnValue;
+}
+
+/**
+ * Writes all time components to member variables for easier parsing and display.
+ * 
+ * This should only be run immediately following a call to getTime()
+ * 
+ * @return void
+ * @author costmo
+ * @since  20180902
+ */
+void timeToVars()
+{
+  //Serial.println( "Time: " + fullTime );
+//  Serial.println( "Second: " + currentSecond );
+
+  int separator1 = fullTime.indexOf( ' ' );
+  int separator2 = fullTime.indexOf( ' ', (separator1 + 1) );
+  int separator3 = fullTime.indexOf( ' ', (separator2 + 1) );
+
+  String hourAsString = fullTime.substring( 0, separator1 );
+  String minuteAsString = fullTime.substring( separator1, separator2 );
+  String secondAsString = fullTime.substring( separator2, separator3 );
+  String meridiemAsString = fullTime.substring( separator3 );
+
+
+  currentHour = hourAsString.toInt();
+  currentMinute = minuteAsString.toInt();
+  int second = secondAsString.toInt();
+
+  currentSecond = second;
+  if( currentSecond >= 60 )
+  {
+    currentSecond = (currentSecond % 60);
+    currentMinute += 1;
+  }
+
+  if( currentMinute >= 60 )
+  {
+    currentMinute = (currentMinute % 60);
+    currentHour += 1;
+  }
+
+  if( currentHour >= 12 )
+  {
+    currentMeridiem = 1;
+    currentHour = (currentHour % 12);
+    if( currentHour == 0 )
+    {
+      currentHour = 11;
+    }
+    currentHour += 1;
+  }
+  else
+  {
+    currentMeridiem = 0;
+  }
+
+  
+//  Serial.print( currentHour);
+//  Serial.print( currentMinute );
+//  Serial.print( currentSecond );
+//  Serial.print( "-" );
+//  Serial.print( currentMeridiem );
+//  Serial.println( "" );
+
+  if( currentHour < 12 && currentMeridiem > 0 )
+  {
+    currentHour = currentHour + 12;
+  }
+
+  String stringMinute = String( currentMinute );
+  if( currentMinute < 10 )
+  {
+    stringMinute = "0" + stringMinute;
+  }
+
+  String timeString = String( currentHour );
+  timeString = timeString + stringMinute;
+  intTime = timeString.toInt();
+
+  //Serial.println( intTime );
+  
+/*
+  Serial.print( "Ticking: " );
+  Serial.print( currentHour );
+  Serial.print( ":" );
+  Serial.print( currentMinute );
+  Serial.print( ":" );
+  Serial.print( currentSecond );
+  Serial.print( " " );
+  Serial.println( currentMeridiem );
+  */
+  
+}
+
+ /**
+ * Send an NTP request to the time server at the given address
+ * 
+ * @return long
+ * @author costmo
+ * @since  20180902
+ * @param  IPAddress    address       A pointer to the IP address of the NTP server
+ */
+unsigned long sendNTPpacket( IPAddress& address )
+{
+  //Serial.println("sending NTP packet...");
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  udp.beginPacket(address, 123); //NTP requests are to port 123
+  udp.write(packetBuffer, NTP_PACKET_SIZE);
+  udp.endPacket();
 }
 
