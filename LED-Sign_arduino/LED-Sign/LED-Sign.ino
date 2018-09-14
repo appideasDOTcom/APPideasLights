5ec16476cad9289faed5b1431448ca4bd067d5fa
Chris Ostmo <chris@appideas.com>
2018-09-13 17:20:37 -0700

    - Updated comments - Some code structure updates - Remove unused code - Added rollcall endpoint

--- LED-Sign_arduino/LED-Sign/LED-Sign.ino
+++ LED-Sign_arduino/LED-Sign/LED-Sign.ino
@@ -3,9 +3,13 @@
 #include <WiFiUdp.h>
 #include <ESP8266WebServer.h>
 #include <WiFiClient.h>
+#include <ArduinoJson.h>
 
 /**
- * A sketch to control two strings of RGBW LED lights from one ESP8266
+ * A sketch to control two strings of RGBW LED lights from one ESP8266.
+ * 
+ * This creates its own wireless access point with an SSID beginning with "appideas-"
+ *   that you need to connect to for configuring this device to connect to your network
  * 
  * @author costmo
  * @since  20180902
@@ -20,10 +24,10 @@ bool wifiConnected = false;
 
 // Setup NTP parameters for syncing with network time
 const int TIMEZONE_OFFSET = 3600; // Number of seconds in an hour
-int inputTimezoneOffset = 0;
-const char* ntpServerName = "time.nist.gov";
+int inputTimezoneOffset = 0; // Offset from UTC - will be input by the user
+const char* ntpServerName = "time.nist.gov"; // NTP server to use
 const int checkInterval = 60 * 30; // how often (in seconds) to query the NTP server for updates (30 minutes)
-// NTP parameters that shouldn't be changed
+// NTP parameters that probably shouldn't be changed
 unsigned int localPort = 2390;      // local port to listen for UDP packets
 IPAddress timeServerIP; // time.nist.gov NTP server address
 const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
@@ -43,7 +47,7 @@ WiFiUDP udp;
 // D7/GPIO13      = 12;
 
 
-// First string of lights. Pins D0 - D3
+// Pin mapping for the first string of lights. Pins D0 - D3
 #define WHITE_LED_FIRST     16
 #define BLUE_LED_FIRST      5
 #define RED_LED_FIRST       0
@@ -64,11 +68,13 @@ IPAddress ip;
 ESP8266WebServer server( serverPort );
 
 // Status variables
+// Temporary holding for current values
 int currentR = 0;
 int currentG = 0;
 int currentB = 0;
 int currentW = 0;
 
+// holding for current values on first and second string of lights
 int currentRFirst = 0;
 int currentGFirst = 0;
 int currentBFirst = 0;
@@ -79,6 +85,7 @@ int currentGSecond = 0;
 int currentBSecond = 0;
 int currentWSecond = 0;
 
+// Global time variables
 String fullTime = String( "" );
 String humanTime = String( "" );
 int intTime = 0;
@@ -98,6 +105,7 @@ void setup()
   Serial.begin( 57600 );
   delay( 100 );
 
+  // Prepare the pins to fire
   pinMode( WHITE_LED_FIRST, OUTPUT );
   pinMode( BLUE_LED_FIRST, OUTPUT );
   pinMode( RED_LED_FIRST, OUTPUT );
@@ -107,8 +115,10 @@ void setup()
   pinMode( BLUE_LED_SECOND, OUTPUT );
   pinMode( RED_LED_SECOND, OUTPUT );
   pinMode( GREEN_LED_SECOND, OUTPUT );
-  
+
+  // Start the local access point
   startSoftAP();
+  // Start the web server and get ready to handle incoming requests
   startWebServer();
 
   // turn all the lights on when the ESP first powers on
@@ -154,7 +164,6 @@ void turnOff( String whichPosition )
  */
 void setColorToLevel( String whichPosition, String color, int level ) 
 { 
-
   int redPin = RED_LED_FIRST;
   int greenPin = GREEN_LED_FIRST;
   int bluePin = BLUE_LED_FIRST;
@@ -225,10 +234,18 @@ void setColorToLevel( String whichPosition, String color, int level )
  */
 void loop() 
 {
-  //nodeServer();
   server.handleClient();
 }
 
+/**
+ * Handler for requests to "/"
+ * 
+ * This should only provide the interface for logging into a network or show a "you are already connected" message
+ * 
+ * @return void
+ * @author costmo
+ * @since  20180913
+ */
 void handleRoot()
 {
   //server.send( 200, "text/html", connectionHtml() );
@@ -242,8 +259,41 @@ void handleRoot()
     Serial.println( "Already connected" );
     server.send( 200, "text/html", connectedHtml() );
   }
+  delay( 100 );
 }
 
+/**
+ * Handler for requests to "/rollcall"
+ * 
+ * This is here so an app can easily scan a network and find nodes
+ * 
+ * @return void
+ * @author costmo
+ * @since  20180913
+ */
+void handleRollCall()
+{
+  String sendValue = "";
+  StaticJsonBuffer<1024> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["status"] = "present";
+  root["mac"] = WiFi.macAddress();
+  root.printTo( sendValue );
+  
+  server.send( 200, "text/html", sendValue );
+}
+
+/**
+ * Handler for requests to "/connect"
+ * 
+ * This should only take requests to login to the network. It can't do anything by itself
+ * 
+ * TODO: Add error checking and re-prompt on unsuccessful connection attempts
+ * 
+ * @return void
+ * @author costmo
+ * @since  20180913
+ */
 void handleConnect()
 {
   String hardSSID = server.arg( "ssid" );
@@ -257,7 +307,8 @@ void handleConnect()
 
   if( !wifiConnected )
   {
-    connectToWifi( hardSSID, hardPassword );
+    ssid = hardSSID;
+    connectToWifi( ssid, hardPassword );
   }
 
   // Get and show the time
@@ -269,8 +320,16 @@ void handleConnect()
   
   server.send( 200, "text/html", connectedHtml() );
   Serial.println( "Connected to WiFi" );
+  delay( 100 );
 }
 
+/**
+ * Start the local access point
+ * 
+ * @return void
+ * @author costmo
+ * @since  20180913
+ */
 void startSoftAP()
 {
   String mac = WiFi.macAddress();
@@ -284,10 +343,49 @@ void startSoftAP()
   Serial.println( getSoftAPStatus() );
 }
 
+/**
+ * Handler for requests to "/network-status"
+ * 
+ * Sends a JSON encoded string to the web browser containing the device's network status
+ * 
+ * @return void
+ * @author costmo
+ * @since  20180913
+ */
+void handleNetworkStatus()
+{
+   server.send( 200, "text/html", getNetworkStatus() );
+}
+
+/**
+ * Handler for requests to "/status"
+ * 
+ * Sends a JSON encoded string to the web browser containing the status device's lights
+ * 
+ * @return void
+ * @author costmo
+ * @since  20180913
+ */
+void handleStatus()
+{
+   server.send( 200, "text/html", getStatus() );
+}
+
+/**
+ * Define the endpoints for a tiny API server for receiving and handling requests
+ * 
+ * @return void
+ * @author costmo
+ * @since  20180902
+ */
 void startWebServer()
 {
   server.on( "/", handleRoot );
   server.on( "/connect", handleConnect );
+  server.on( "/network-status", handleNetworkStatus );
+  server.on( "/status", handleStatus );
+  server.on( "/control", handleControl );
+  server.on( "/rollcall", handleRollCall );
   server.begin();
 }
 
@@ -330,38 +428,6 @@ void printWifiStatus()
   ip = WiFi.localIP();
   Serial.print( "IP Address: " );
   Serial.println( ip );
-}
-
-/**
- * Prints the HTML header for output
- * 
- * @return void
- * @author costmo
- * @since  20180902
- * @param  WifiClient   client    The wifi client
- */
-void htmlHeader( WiFiClient client )
-{
-    client.println( "HTTP/1.1 200 OK" );
-    client.println ("Content-Type: text/html" );
-    client.println( "Connection: close" );  // the connection will be closed after completion of the response
-    client.println( "Cache-Control: no-store" );
-    client.println();
-    client.println( "<!DOCTYPE HTML>" );
-    client.println( "<html>" );
-}
-
- /**
- * Prints the HTML footer for output
- * 
- * @return void
- * @author costmo
- * @since  20180902
- * @param  WifiClient   client    The wifi client
- */
-void htmlFooter( WiFiClient client )
-{
-    client.println( "</html>" );
 }
  
 /**
@@ -442,220 +508,60 @@ float getRatioForColor( String whichPosition, String color )
   return (float) ((float)currentValue / (float)1024);
 }
 
-/**
- * A tiny API server for receiving and handling requests
- * 
- * @return void
- * @author costmo
- * @since  20180902
- */
-void nodeServer()
+void handleControl()
 {
-//   WiFiClient client = server.available();
-//   if( client )
-//   {
-//      String requestString; 
-//      boolean currentLineIsBlank = true;
-//      while( client.connected() )
-//      {
-//        if( client.available() )
-//        {
-//          char c = client.read();
-//          requestString += c;
-//
-//          // request is complete, parse it
-//          if( c == '\n' && currentLineIsBlank )
-//          {
-//              htmlHeader( client );
-//
-//              String requestPosition = "first";
-//              String requestColor = "all";
-//              int requestLevel = 1024;
-//
-//              if( requestString.indexOf( "?action=id" ) > 0 )
-//              {
-//                byte mac[6];
-//                WiFi.macAddress( mac );
-//                // Using client.write multiple times caused scrolling output. Building a string causes instant output on completion, which is preferable
-////                String output = nodeType;
-////                output += "|";
-//                String output = String( mac[0], HEX );
-//                output +=  ":" ;
-//                output += String( mac[1], HEX );
-//                output +=  ":" ;
-//                output += String( mac[2], HEX );
-//                output +=  ":" ;
-//                output += String( mac[3], HEX );
-//                output +=  ":" ;
-//                output += String( mac[4], HEX );
-//                output +=  ":" ;
-//                output += String( mac[5], HEX );
-//                output += "|";
-//                output += WiFi.localIP();
-////                output += "|";
-////                output += nodeVersion;
-//                client.println( output );
-//                return; // We don't want to go below this if the request was for "id"
-//              }
-//              
-//              // parse the incoming request and do the work
-//              // p = "position" = first|second
-//              // c = "color" = red|green|blue|white|all
-//              // l ="level" = 0|25|50|75|100
-//              if( requestString.indexOf( "?p=second" ) > 0 || requestString.indexOf( "&p=second" ) > 0 )
-//              {
-//                requestPosition = "second";
-//              }
-//              if( requestString.indexOf( "?c=r" ) > 0 || requestString.indexOf( "&c=r" ) > 0 )
-//              {
-//                requestColor = "red";
-//              }
-//              if( requestString.indexOf( "?c=g" ) > 0 || requestString.indexOf( "&c=g" ) > 0 )
-//              {
-//                requestColor = "green";
-//              }
-//              if( requestString.indexOf( "?c=b" ) > 0 || requestString.indexOf( "&c=b" ) > 0 )
-//              {
-//                requestColor = "blue";
-//              }
-//              if( requestString.indexOf( "?c=w" ) > 0 || requestString.indexOf( "&c=w" ) > 0 )
-//              {
-//                requestColor = "white";
-//              }
-//              
-//              if( requestString.indexOf( "?l=0" ) > 0 || requestString.indexOf( "&l=0" ) > 0 )
-//              {
-//                requestLevel = 0;
-//              }
-//              if( requestString.indexOf( "?l=25" ) > 0 || requestString.indexOf( "&l=25" ) > 0 )
-//              {
-//                requestLevel = 256;
-//              }
-//              if( requestString.indexOf( "?l=50" ) > 0 || requestString.indexOf( "&l=50" ) > 0 )
-//              {
-//                requestLevel = 512;
-//              }
-//              if( requestString.indexOf( "?l=75" ) > 0 || requestString.indexOf( "&l=75" ) > 0 )
-//              {
-//                requestLevel = 768;
-//              }
-//
-//
-//              if( requestColor == "all"  )
-//              {
-//                if( requestLevel == 100 )
-//                {
-//                  turnOn( requestPosition );
-//                }
-//                else if( requestLevel == 0 )
-//                {
-//                  turnOff( requestPosition );
-//                }
-//                else
-//                {
-//                  setColorToLevel( requestPosition, "red", requestLevel );
-//                  setColorToLevel( requestPosition, "green", requestLevel );
-//                  setColorToLevel( requestPosition, "blue", requestLevel );
-//                  setColorToLevel( requestPosition, "white", requestLevel );
-//                }
-//              }
-//              else
-//              {
-//                setColorToLevel( requestPosition, requestColor, requestLevel );
-//              }
-//
-//              client.println( getValues() );
-//              
-//              htmlFooter( client );
-//              break;
-//          } // if( c == '\n' && currentLineIsBlank )
-//          if( c == '\n' ) 
-//          {
-//            currentLineIsBlank = true;
-//          } 
-//          else if( c != '\r' ) 
-//          {
-//            // you've gotten a character on the current line
-//            currentLineIsBlank = false;
-//          }
-//          
-//        } // if( client.available() )
-//      } // while( client.connected() )
-//
-//    // give the web browser time to receive the data
-//    delay( 1 );
-//
-//    // close the connection:
-//    client.stop();
-//   } // if( client )
-} // void nodeServer()
+  // parse the incoming request and do the work
+  // p = "position" = first|second
+  // c = "color" = red|green|blue|white|all
+  // l ="level" = 0|25|50|75|100
 
-/**
- * Gets an HTML string with the values for all lights and positions
- * 
- * @return String
- * @author costmo
- * @since  20180902
- */
-String getValues()
-{
-  String output = "<p>First Red value: ";
-                output += getLevelForColor( "first", "red" );
-                output += "<br>";
-                output += "First Red ratio: ";
-                output += getRatioForColor( "first", "red" );
-                output += "&nbsp;</p>";
+  String lightPosition = server.arg( "p" );
+  String lightColor = server.arg( "c" );
+  int lightLevel = server.arg( "l" ).toInt();
 
-                output += "<p>First Green value: ";
-                output += getLevelForColor( "first", "green" );
-                output += "<br>";
-                output += "First Green ratio: ";
-                output += getRatioForColor( "first", "green" );
-                output += "&nbsp;</p>";
+  Serial.println( lightPosition );
+  Serial.println( lightColor );
+  Serial.println( lightLevel );
 
-                output += "<p>First Blue value: ";
-                output += getLevelForColor( "first", "blue" );
-                output += "<br>";
-                output += "First Blue ratio: ";
-                output += getRatioForColor( "first", "blue" );
-                output += "&nbsp;</p>";
+  String requestPosition = "first";
+  String requestColor = lightColor;
+  int requestLevel = 1024;
 
-                output += "<p>First White value: ";
-                output += getLevelForColor( "first", "white" );
-                output += "<br>";
-                output += "First White ratio: ";
-                output += getRatioForColor( "first", "white" );
-                output += "&nbsp;</p>";
+  if( lightPosition.equals( "second" ) )
+  {
+    requestPosition = "second";
+  }
 
-                output += "<p>&nbsp;</p><p>Second Red value: ";
-                output += getLevelForColor( "second", "red" );
-                output += "<br>";
-                output += "Second Red ratio: ";
-                output += getRatioForColor( "second", "red" );
-                output += "&nbsp;</p>";
+  if( lightLevel < 25 )
+  {
+    requestLevel = 0;
+  }
+  else if( lightLevel == 25 )
+  {
+    requestLevel = 256;
+  }
+  else if( lightLevel == 50 )
+  {
+    requestLevel = 512;
+  }
+  else if( lightLevel == 75 )
+  {
+    requestLevel = 768;
+  }
 
-                output += "<p>Second Green value: ";
-                output += getLevelForColor( "second", "green" );
-                output += "<br>";
-                output += "Second Green ratio: ";
-                output += getRatioForColor( "second", "green" );
-                output += "&nbsp;</p>";
-
-                output += "<p>Secondirst Blue value: ";
-                output += getLevelForColor( "second", "blue" );
-                output += "<br>";
-                output += "Second Blue ratio: ";
-                output += getRatioForColor( "second", "blue" );
-                output += "&nbsp;</p>";
-
-                output += "<p>Second White value: ";
-                output += getLevelForColor( "second", "white" );
-                output += "<br>";
-                output += "Second White ratio: ";
-                output += getRatioForColor( "second", "white" );
-                output += "&nbsp;</p>";
-
-  return output;
+  if( requestColor == "all"  )
+  {
+    setColorToLevel( requestPosition, "red", requestLevel );
+    setColorToLevel( requestPosition, "green", requestLevel );
+    setColorToLevel( requestPosition, "blue", requestLevel );
+    setColorToLevel( requestPosition, "white", requestLevel );
+  }
+  else
+  {
+    setColorToLevel( requestPosition, requestColor, requestLevel );
+  }
+  delay( 100 ); // give the web server a small amount of time to buffer and send
+  server.send( 200, "text/html", getStatus() );
 }
 
 /**
@@ -764,6 +670,8 @@ String getTime()
  * 
  * This should only be run immediately following a call to getTime()
  * 
+ * TODO: Make minutes and seconds < 10 zero-padded
+ * 
  * @return void
  * @author costmo
  * @since  20180902
@@ -860,6 +768,89 @@ unsigned long sendNTPpacket( IPAddress& address )
   udp.endPacket();
 }
 
+/**
+ * Gets the network status and returns info as a JSON-encoded string
+ * 
+ * @return String
+ * @author costmo
+ * @since  20180913
+ */
+String getNetworkStatus()
+{
+  String returnValue = "";
+
+  StaticJsonBuffer<1024> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  if( wifiConnected )
+  {
+    root["soft"] = "disconnected";
+    root["hard"] = "connected";
+    root["ip"] = WiFi.localIP().toString();
+  }
+  else
+  {
+    root["soft"] = "connected";
+    root["hard"] = "disconnected";
+    root["ip"] = softIP;
+  }
+  root["ssid"] = ssid;
+
+  root.printTo( returnValue );
+
+  return returnValue;
+}
+
+/**
+ * Gets the status of each color on each string of lights and returns info as a JSON-encoded string
+ * 
+ * @return String
+ * @author costmo
+ * @since  20180913
+ */
+String getStatus()
+{
+  String returnValue = "";
+
+  StaticJsonBuffer<1024> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+
+  JsonArray& fv = root.createNestedArray( "first-values" );
+  JsonArray& fr = root.createNestedArray( "first-ratios" );
+  JsonArray& sv = root.createNestedArray( "second-values" );
+  JsonArray& sr = root.createNestedArray( "second-ratios" );
+
+  fv.add( getLevelForColor( "first", "red" ) );
+  fv.add( getLevelForColor( "first", "green" ) );
+  fv.add( getLevelForColor( "first", "blue" ) );
+  fv.add( getLevelForColor( "first", "white" ) );
+
+  fr.add( getRatioForColor( "first", "red" ) );
+  fr.add( getRatioForColor( "first", "green" ) );
+  fr.add( getRatioForColor( "first", "blue" ) );
+  fr.add( getRatioForColor( "first", "white" ) );
+
+  sv.add( getLevelForColor( "second", "red" ) );
+  sv.add( getLevelForColor( "second", "green" ) );
+  sv.add( getLevelForColor( "second", "blue" ) );
+  sv.add( getLevelForColor( "second", "white" ) );
+
+  sr.add( getRatioForColor( "second", "red" ) );
+  sr.add( getRatioForColor( "second", "green" ) );
+  sr.add( getRatioForColor( "second", "blue" ) );
+  sr.add( getRatioForColor( "second", "white" ) );
+
+  root.printTo( returnValue );
+
+  return returnValue;
+}
+
+/**
+ * Gets the SSIS and IP address of the local access point
+ * 
+ * @return String
+ * @author costmo
+ * @since  20180913
+ */
 String getSoftAPStatus()
 {
   String returnValue = "\n";
@@ -869,6 +860,13 @@ String getSoftAPStatus()
   return returnValue;
 }
 
+/**
+ * HTML form for connecting to a WiFi network
+ * 
+ * @return const String
+ * @author costmo
+ * @since  20180913
+ */
 const String connectionHtml()
 {
   const String returnValue =
@@ -900,6 +898,13 @@ const String connectionHtml()
   return returnValue;
 }
 
+/**
+ * HTML to show when a user is already connected and is on a page that offers or processes connections
+ * 
+ * @return const String
+ * @author costmo
+ * @since  20180913
+ */
 const String connectedHtml()
 {
   const String returnValue =
